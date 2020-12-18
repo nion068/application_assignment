8b2b99f7d0f0fa5032c14e988b25b71ca78d8494
nion068 <mu.islam34@gmail.com>
2020-12-19 01:24:16 +0600

    frontend initial setup done - html css urls

--- Assignment/apply/views.py
+++ Assignment/apply/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def login(request):
+    return render(request, 'apply/login.html')
+
+def details(request):
+    return render(request, 'apply/details.html')
